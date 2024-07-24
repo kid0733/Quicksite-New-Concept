5fefd19eec2701d049f288ca08f2eaada23289bd
Siddhant Karki <siddhantskarki@gmail.com>
2024-07-24 15:01:16 +1000

    animations + carousel-init

--- script.js
+++ script.js
@@ -2,16 +2,33 @@ VANTA.TRUNK({
     el: "#background",
     mouseControls: true,
     touchControls: true,
-    gyroControls: false,
+    gyroControls: true,
     minHeight: 200.0,
     minWidth: 200.0,
-    scale: 0.5,
+    scale: 0.8,
     scaleMobile: 1.0,
     color: 0xa9d6b4,
     backgroundColor: 0xe8f2ea,
     spacing: 10,
     chaos: 1.5
 });
+
+
+
+// IMAGE ROTATION
+const card = document.querySelector(".card__inner");
+
+window.addEventListener("scroll", function () {
+    // Get the scroll position
+    const scrollPosition = window.scrollY;
+
+    // Calculate rotation angle (adjust the factor as needed)
+    const rotationAngle = scrollPosition * 0.1;
+
+    // Apply the rotation transform
+    card.style.transform = `rotateX(${rotationAngle}deg)`;
+});
+
 
 // 
 // GSAP
@@ -20,9 +37,16 @@ VANTA.TRUNK({
 window.onload = function () {
 
     var timeline = new TimelineMax();
+    timeline.from("#heroImg", 2, {
+        duration: 1.5,
+        ease: "power4.inOut",
+        y: 500
+    }, 0)
     timeline.from("#title,#title1", 1, { scale: 0.5 }, 0)
     timeline.from("#heroText", 5, { opacity: 0 }, 0)
 
 
 }
+
+
 
