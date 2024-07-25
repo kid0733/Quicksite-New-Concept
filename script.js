cb0bb83b66cc771b0f2f9ef60aa7a7da18269c60
Siddhant Karki <siddhantskarki@gmail.com>
2024-07-25 20:37:04 +1000

    Responsive View Showcase

--- script.js
+++ script.js
@@ -1,5 +1,9 @@
-
-
+//NAV TOGGLE
+function toggleNav() {
+    const navItems = document.querySelector('.nav-items');
+    navItems.classList.toggle('show');
+}
+//VANTA BG
 
 VANTA.TRUNK({
     el: "#background",
@@ -23,10 +27,10 @@ const card = document.querySelector(".card__inner");
 
 window.addEventListener("scroll", function () {
     // Get the scroll position
-    const scrollPosition = window.scrollY;
+    const scrollPosition = window.scrollY*1.25;
 
     // Calculate rotation angle (adjust the factor as needed)
-    const rotationAngle = scrollPosition * 0.1;
+    const rotationAngle = scrollPosition * 0.08;
 
     // Apply the rotation transform
     card.style.transform = `rotateX(${rotationAngle}deg)`;
@@ -34,23 +38,25 @@ window.addEventListener("scroll", function () {
 
 
 // 
-// GSAP
+// GSAP Animations 
 // 
 
 window.onload = function () {
     gsap.registerPlugin(ScrollTrigger);
 
     var timeline = gsap.timeline();
+    // Hero Image
     timeline.from("#heroImg", {
         duration: 1.5,
         ease: "power4.inOut",
         y: 500
     })
+        // Hero Title
         .from("#title,#title1", { scale: 0.5, duration: 1 }, 0)
         .from("#heroText", { opacity: 0, duration: 5 }, 0);
 
     // Adding ScrollTrigger for #imgCarouselText
-    gsap.from("#imgCarouselText,#imgCarousel", {
+    gsap.from("#imgCarouselText", {
 
         scale: 0.5,
         ease: "power1.inOut",
@@ -62,11 +68,99 @@ window.onload = function () {
         },
         duration: 1
     });
+    // Features Carousel
+    gsap.from("#featuresCarouselTitle", {
+
+        scale: 0.5,
+        ease: "power1.inOut",
+        scrollTrigger: {
+            trigger: "#featuresCarouselTitle",
+            start: "top 80%", // when the top of the element is 80% from the top of the viewport
+            end: "bottom 20%", // when the bottom of the element is 20% from the top of the viewport
+            toggleActions: "play none none none"
+        },
+        duration: 1
+    });
+    // Features Carousel Text
+    gsap.from("#featuresCarouselText", {
+        opacity: 0,
+        y: 100,
+        scale: 0.5,
+        delay:0.5,
+        ease: "power1.inOut",
+        scrollTrigger: {
+            trigger: "#featuresCarouselTitle",
+            start: "top 80%", // when the top of the element is 80% from the top of the viewport
+            end: "bottom 20%", // when the bottom of the element is 20% from the top of the viewport
+            toggleActions: "play none none none"
+        },
+        duration: 1
+    });
+    // Features Carousel
+    gsap.from("#features_intro_slider_container", {
+        opacity: 0,
+        y: 100,
+        scale: 0.5,
+        ease: "power1.inOut",
+        delay: 1,
+        scrollTrigger: {
+            trigger: "#featuresCarouselTitle",
+            start: "top 80%", // when the top of the element is 80% from the top of the viewport
+            end: "bottom 20%", // when the bottom of the element is 20% from the top of the viewport
+            toggleActions: "play none none none"
+        },
+        duration: 1
+    });
+    //Mobile Carousel
+    gsap.from("#mobileCarouselTitle", {
+        opacity: 0,
+        scale: 0.5,
+        ease: "power1.inOut",
+        scrollTrigger: {
+            trigger: "#mobileCarouselTitle",
+            start: "top 80%", // when the top of the element is 80% from the top of the viewport
+            end: "bottom 20%", // when the bottom of the element is 20% from the top of the viewport
+            toggleActions: "play none none none"
+        },
+        duration: 1
+    });
+    gsap.from("#mobileCarouselText", {
+        opacity: 0,
+        scale: 0.5,
+        ease: "power1.inOut",
+        delay: 0.5,
+        scrollTrigger: {
+            trigger: "#mobileCarouselTitle",
+            start: "top 80%", // when the top of the element is 80% from the top of the viewport
+            end: "bottom 20%", // when the bottom of the element is 20% from the top of the viewport
+            toggleActions: "play none none none"
+        },
+        duration: 1
+    });
+    
+    ScrollTrigger.create({
+        start: "top top",
+        onUpdate: (self) => {
+            if (self.scroll() > 500) {
+                gsap.to(".nav-container", {
+                    backgroundColor: "#033810",
+                    duration: 0.3,
+                    opacity: 0.95,
+                    blur:20
+                });
+            } else {
+                gsap.to(".nav-container", {
+                    backgroundColor: "rgba(4, 71, 80, 0.2)",
+                    duration: 0.3,
+                });
+            }
+        }
+    });
 }
 
 
 // 
-// Img Carousel
+// Template Image Carousel
 // 
 document.addEventListener("DOMContentLoaded", () => {
     const marquee = document.querySelector(".marquee-inner");
@@ -101,28 +195,69 @@ document.addEventListener("DOMContentLoaded", () => {
 });
 
 
+//Mobile View Carousel
 
-// Features
+document.addEventListener("DOMContentLoaded", () => {
+    const carousel = document.querySelector(".mobile-carousel-inner");
+    const speed = 1; // Scrolling Speed
+    let scrollAmount = 0;
+    let isHovered = false;
+
+    // Duplicates the content
+    const carouselContent = carousel.innerHTML;
+    carousel.innerHTML += carouselContent;
+
+    const startScrolling = () => {
+        if (!isHovered) {
+            scrollAmount -= speed;
+            if (Math.abs(scrollAmount) >= carousel.scrollWidth / 2) {
+                scrollAmount = 0;
+            }
+            carousel.style.transform = `translateX(${scrollAmount}px)`;
+        }
+        requestAnimationFrame(startScrolling);
+    };
+
+    carousel.addEventListener("mouseover", () => {
+        isHovered = true;
+    });
+
+    carousel.addEventListener("mouseout", () => {
+        isHovered = false;
+    });
+
+    startScrolling();
+});
+
+
+
+// Features Intro Slider
 document.addEventListener('DOMContentLoaded', () => {
-    const teamSliderInner = document.querySelector('.team-slider-inner');
-    const items = document.querySelectorAll('.team-slider-item');
-    const prevButton = document.getElementById('prev');
-    const nextButton = document.getElementById('next');
+    const featuresIntroSliderInner = document.querySelector('.features-intro-slider-inner');
+    const items = document.querySelectorAll('.features-intro-slider-item');
+    const prevButton = document.getElementById('features_intro_prev');
+    const nextButton = document.getElementById('features_intro_next');
     let currentIndex = 0;
 
-    const updateTeamSlider = () => {
-        const offset = -currentIndex * 100;
-        teamSliderInner.style.transform = `translateX(${offset}%)`;
+    const updateFeaturesIntroSlider = () => {
+        const itemWidth = items[0].clientWidth;
+        const offset = -currentIndex * itemWidth;
+        featuresIntroSliderInner.style.transform = `translateX(${offset}px)`;
     };
 
     prevButton.addEventListener('click', () => {
         currentIndex = currentIndex > 0 ? currentIndex - 1 : items.length - 1;
-        updateTeamSlider();
+        updateFeaturesIntroSlider();
     });
 
     nextButton.addEventListener('click', () => {
         currentIndex = currentIndex < items.length - 1 ? currentIndex + 1 : 0;
-        updateTeamSlider();
+        updateFeaturesIntroSlider();
     });
+
+    window.addEventListener('resize', updateFeaturesIntroSlider);
+    updateFeaturesIntroSlider();
 });
+
+
 
