91d16b21855f24e3e12c10bae56847849dd09247
Siddhant Karki <siddhantskarki@gmail.com>
2024-07-27 13:07:34 +1000

    new carousel

--- script.js
+++ script.js
@@ -178,87 +178,11 @@ window.onload = function () {
 // 
 // Template Image Carousel
 // 
-document.addEventListener("DOMContentLoaded", function () {
-    "use strict";
-
-    var carousel = document.getElementsByClassName('template-carousel')[0],
-        slider = carousel.getElementsByClassName('template-carousel__slider')[0],
-        items = carousel.getElementsByClassName('template-carousel__slider__item'),
-        prevBtn = carousel.getElementsByClassName('template-carousel__prev')[0],
-        nextBtn = carousel.getElementsByClassName('template-carousel__next')[0];
-
-    var width, height, totalWidth, margin = 20,
-        currIndex = 0,
-        interval, intervalTime = 4000;
-
-    function init() {
-        resize();
-        move(Math.floor(items.length / 2));
-        bindEvents();
-
-        timer();
-    }
-
-    function resize() {
-        width = Math.max(window.innerWidth * .25, 275);
-        height = window.innerHeight * 0.5;
-        totalWidth = width * items.length;
-
-        slider.style.width = totalWidth + "px";
-
-        for (var i = 0; i < items.length; i++) {
-            let item = items[i];
-            item.style.width = (width - (margin * 2)) + "px";
-            item.style.height = 40 + "vh";
-        }
-    }
-
-    function move(index) {
-
-        if (index < 1) index = items.length;
-        if (index > items.length) index = 1;
-        currIndex = index;
-
-        for (var i = 0; i < items.length; i++) {
-            let item = items[i],
-                box = item.getElementsByClassName('item__3d-frame')[0];
-            if (i == (index - 1)) {
-                item.classList.add('template-carousel__slider__item--active');
-                box.style.transform = "perspective(1200px)";
-            } else {
-                item.classList.remove('template-carousel__slider__item--active');
-                box.style.transform = "perspective(1200px) rotateY(" + (i < (index - 1) ? 40 : -40) + "deg)";
-            }
-        }
-
-        slider.style.transform = "translate3d(" + ((index * -width) + (width / 2) + window.innerWidth / 2) + "px, 0, 0)";
-    }
-
-    function timer() {
-        clearInterval(interval);
-        interval = setInterval(() => {
-            move(++currIndex);
-        }, intervalTime);
-    }
-
-    function prev() {
-        move(--currIndex);
-        timer();
-    }
-
-    function next() {
-        move(++currIndex);
-        timer();
-    }
-
-    function bindEvents() {
-        window.onresize = resize;
-        prevBtn.addEventListener('click', () => { prev(); });
-        nextBtn.addEventListener('click', () => { next(); });
-    }
-
-    init();
-});
+const embla = EmblaCarousel(document.querySelector('.embla'), {
+    loop: true,
+    align: 'start',
+    skipSnaps: true,
+})
 
 
 
